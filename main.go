571aac4f0198fd18f21546d44593f68d36026672
Vali Maris <marisvali@yahoo.com>
2024-05-28 08:40:00 +0300

    make energy beam block against obstacles

--- main.go
+++ main.go
@@ -42,6 +42,7 @@ type Gui struct {
 	leftClickPos  Pt
 	rightClickPos Pt
 	beamIdx       Int
+	beam          Line
 }
 
 func Check(e error) {
@@ -137,6 +138,10 @@ func (g *Gui) Update() error {
 		if dist < 200 {
 			// Hit enemy.
 			g.beamIdx = I(15)
+			g.beam = Line{g.TileToScreen(g.world.Player.Pos), g.TileToScreen(g.world.Enemy.Pos)}
+			if intersects, pt := g.LineObstaclesIntersection(g.beam); intersects {
+				g.beam.End = pt
+			}
 		} else {
 			// Translate from screen coordinates to grid coordinates.
 			newPos := IPt(
@@ -149,7 +154,7 @@ func (g *Gui) Update() error {
 	}
 
 	// Move the enemy.
-	if g.world.TimeStep.Mod(I(4)).Eq(ZERO) {
+	if g.world.TimeStep.Mod(I(40)).Eq(ZERO) {
 		path := g.pathfinding.FindPath(g.world.Enemy.Pos, g.world.Player.Pos)
 		if len(path) > 1 {
 			g.world.Enemy.Pos = path[1]
@@ -157,6 +162,116 @@ func (g *Gui) Update() error {
 	}
 
 	return nil
+}
+
+func LineVerticalLineIntersection(l, vert Line) (bool, Pt) {
+	// Check if the Lines even intersect.
+
+	// Check if l's min X is at the right of vertX.
+	minX, maxX := MinMax(l.Start.X, l.End.X)
+	vertX := vert.Start.X // we assume vert.Start.X == vert.End.X
+
+	if minX.Gt(vertX) {
+		return false, Pt{}
+	}
+
+	// Or if l's max X is at the left of vertX.
+	if maxX.Lt(vertX) {
+		return false, Pt{}
+	}
+
+	//// Check if l's minY is under the vertMaxY.
+	//minY, maxY := MinMax(l.Start.Y, l.End.Y)
+	//vertMinY, vertMaxY := MinMax(vert.Start.Y, vert.End.Y)
+	//
+	//if minY.Gt(vertMaxY) {
+	//	return false, Pt{}
+	//}
+	//
+	//// Or if l's max Y is above vertMinY.
+	//if maxY.Lt(vertMinY) {
+	//	return false, Pt{}
+	//}
+
+	vertMinY, vertMaxY := MinMax(vert.Start.Y, vert.End.Y)
+
+	// We know the intersection point will have the X coordinate equal to vertX.
+	// We just need to compute the Y coordinate.
+	// We have to move along the Y axis the same proportion that we moved along
+	// the X axis in order to get to the intersection point.
+
+	//factor := (vertX - l.Start.X) / (l.End.X - l.Start.X) // will always be positive
+	//y := l.Start.Y + factor * (l.End.Y - l.Start.Y) // l.End.Y - l.Start.Y will
+	// have the proper sign so that Y gets updated in the right direction
+	//y := l.Start.Y + (vertX - l.Start.X) / (l.End.X - l.Start.X) * (l.End.Y - l.Start.Y)
+	//y := l.Start.Y + (vertX - l.Start.X) * (l.End.Y - l.Start.Y) / (l.End.X - l.Start.X)
+	var y Int
+	if l.End.X.Eq(l.Start.X) {
+		y = l.Start.Y
+	} else {
+		y = l.Start.Y.Plus((vertX.Minus(l.Start.X)).Times(l.End.Y.Minus(l.Start.Y)).DivBy(l.End.X.Minus(l.Start.X)))
+	}
+
+	if y.Lt(vertMinY) || y.Gt(vertMaxY) {
+		return false, Pt{}
+	} else {
+		return true, Pt{vertX, y}
+	}
+}
+
+func LineHorizontalLineIntersection(l, horiz Line) (bool, Pt) {
+	// Check if the Lines even intersect.
+
+	// Check if l's minY is under the vertY.
+	minY, maxY := MinMax(l.Start.Y, l.End.Y)
+	vertY := horiz.Start.Y // we assume vert.Start.Y == vert.End.Y
+
+	if minY.Gt(vertY) {
+		return false, Pt{}
+	}
+
+	// Or if l's max Y is above vertY.
+	if maxY.Lt(vertY) {
+		return false, Pt{}
+	}
+
+	//// Check if l's min X is at the right of vertMaxX.
+	//minX, maxX := MinMax(l.Start.X, l.End.X)
+	//vertMinX, vertMaxX := MinMax(horiz.Start.X, horiz.End.X)
+	//
+	//if minX.Gt(vertMaxX) {
+	//	return false, Pt{}
+	//}
+	//
+	//// Or if l's max X is at the left of vertMinX.
+	//if maxX.Lt(vertMinX) {
+	//	return false, Pt{}
+	//}
+
+	vertMinX, vertMaxX := MinMax(horiz.Start.X, horiz.End.X)
+
+	// We know the intersection point will have the Y coordinate equal to vertY.
+	// We just need to compute the X coordinate.
+	// We have to move along the X axis the same proportion that we moved along
+	// the Y axis in order to get to the intersection point.
+
+	//factor := (vertY - l.Start.Y) / (l.End.Y - l.Start.Y) // will always be positive
+	//x := l.Start.X + factor * (l.End.X - l.Start.X) // l.End.X - l.Start.X will
+	// have the proper sign so that Y gets updated in the right direction
+	//x := l.Start.X + (vertY - l.Start.Y) / (l.End.Y - l.Start.Y) * (l.End.X - l.Start.X)
+	//x := l.Start.X + (vertY - l.Start.Y) * (l.End.X - l.Start.X) / (l.End.Y - l.Start.Y)
+	var x Int
+	if l.End.Y.Eq(l.Start.Y) {
+		x = l.Start.X
+	} else {
+		x = l.Start.X.Plus((vertY.Minus(l.Start.Y)).Times(l.End.X.Minus(l.Start.X)).DivBy(l.End.Y.Minus(l.Start.Y)))
+	}
+
+	if x.Lt(vertMinX) || x.Gt(vertMaxX) {
+		return false, Pt{}
+	} else {
+		return true, Pt{x, vertY}
+	}
 }
 
 func colorHex(hexVal int) color.Color {
@@ -196,6 +311,84 @@ type Circle struct {
 type Square struct {
 	Center Pt
 	Size   Int
+}
+
+func GetClosestPoint(pts []Pt, refPt Pt) (bool, Pt) {
+	if len(pts) == 0 {
+		return false, Pt{}
+	}
+
+	// Find the point closest to the reference point.
+	minDist := refPt.SquaredDistTo(pts[0])
+	minIdx := 0
+	for idx := 1; idx < len(pts); idx++ {
+		dist := refPt.SquaredDistTo(pts[idx])
+		if dist.Lt(minDist) {
+			minDist = dist
+			minIdx = idx
+		}
+	}
+	return true, pts[minIdx]
+}
+
+func LineSquareIntersection(l Line, s Square) (bool, Pt) {
+	half := s.Size.DivBy(TWO)
+	p1 := Pt{s.Center.X.Minus(half), s.Center.Y.Minus(half)}
+	p2 := Pt{s.Center.X.Plus(half), s.Center.Y.Minus(half)}
+	p3 := Pt{s.Center.X.Plus(half), s.Center.Y.Plus(half)}
+	p4 := Pt{s.Center.X.Minus(half), s.Center.Y.Plus(half)}
+
+	l1 := Line{p1, p2}
+	l2 := Line{p2, p3}
+	l3 := Line{p3, p4}
+	l4 := Line{p4, p1}
+
+	ipts := []Pt{}
+	if intersects, ipt := LineHorizontalLineIntersection(l, l1); intersects {
+		ipts = append(ipts, ipt)
+	}
+	if intersects, ipt := LineVerticalLineIntersection(l, l2); intersects {
+		ipts = append(ipts, ipt)
+	}
+	if intersects, ipt := LineHorizontalLineIntersection(l, l3); intersects {
+		ipts = append(ipts, ipt)
+	}
+	if intersects, ipt := LineVerticalLineIntersection(l, l4); intersects {
+		ipts = append(ipts, ipt)
+	}
+
+	return GetClosestPoint(ipts, l.Start)
+}
+
+func EqualFloats(f1, f2 float64) bool {
+	return math.Abs(f1-f2) < 0.000001
+}
+
+func (g *Gui) LineObstaclesIntersection(l Line) (bool, Pt) {
+	sz := g.screenSize
+	numX := g.world.Obstacles.NCols().ToInt()
+	numY := g.world.Obstacles.NRows().ToInt()
+	blockWidth := sz.X.ToFloat64() / float64(numX)
+	blockHeight := sz.Y.ToFloat64() / float64(numY)
+
+	rows := g.world.Obstacles.NRows()
+	cols := g.world.Obstacles.NCols()
+	ipts := []Pt{}
+	for y := ZERO; y.Lt(rows); y.Inc() {
+		for x := ZERO; x.Lt(cols); x.Inc() {
+			if !g.world.Obstacles.Get(y, x).IsZero() {
+				if !EqualFloats(blockWidth, blockHeight) {
+					panic(fmt.Errorf("blocks are not squares"))
+				}
+				s := Square{g.TileToScreen(Pt{x, y}), I(int(blockWidth))}
+				if intersects, ipt := LineSquareIntersection(l, s); intersects {
+					ipts = append(ipts, ipt)
+				}
+			}
+		}
+	}
+
+	return GetClosestPoint(ipts, l.Start)
 }
 
 func DrawLine(screen *ebiten.Image, l Line, color color.Color) {
@@ -277,7 +470,7 @@ func (g *Gui) Draw(screen *ebiten.Image) {
 
 	// Draw beam.
 	if g.beamIdx.Gt(ZERO) {
-		DrawLine(screen, Line{g.TileToScreen(g.world.Player.Pos), g.TileToScreen(g.world.Enemy.Pos)}, intToCol(4))
+		DrawLine(screen, g.beam, intToCol(4))
 		g.beamIdx.Dec()
 	}
 
