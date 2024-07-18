0bb502b6964cb1f35cca5e355e2f97c7f86dd3a8
Vali Maris <marisvali@yahoo.com>
2024-07-19 00:07:56 +0300

    update version

--- analysis/main.go
+++ analysis/main.go
@@ -20,9 +20,9 @@ func main() {
 		// start := os.Args[2]
 		// end := os.Args[3]
 		// version := os.Args[4]
-		start := "2024-07-14 10:38:51"
-		end := "2024-07-14 11:01:04"
-		version := "5"
+		start := "2024-07-14 10:01:21"
+		end := "2024-07-14 10:17:16"
+		version := "4"
 		UpdateVersion(start, end, version)
 	}
 }
