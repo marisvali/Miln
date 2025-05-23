96f33b26d0b6f1d9ca0643df2df619f9495d3004
Vali Maris <marisvali@yahoo.com>
2025-05-23 08:33:43 +0300

    set version to 11

--- upload-to-playful-patterns.ps1
+++ upload-to-playful-patterns.ps1
@@ -1,9 +1,9 @@
 $Env:GOOS = 'js'
 $Env:GOARCH = 'wasm'
-go build -o miln999.wasm github.com/marisvali/miln
+go build -o miln11.wasm github.com/marisvali/miln
 Remove-Item Env:GOOS
 Remove-Item Env:GOARCH
 
 $client = New-Object System.Net.WebClient
 $client.Credentials = New-Object System.Net.NetworkCredential($Env:MILN_FTP_USER, $Env:MILN_FTP_PASSWORD)
-$client.UploadFile("ftp://ftp.playful-patterns.com/public_html/miln999.wasm", "miln999.wasm")
+$client.UploadFile("ftp://ftp.playful-patterns.com/public_html/miln11.wasm", "miln11.wasm")
