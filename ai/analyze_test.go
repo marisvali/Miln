13e52fbfa443bdedb615c184b24a00970e996b17
Vali Maris <marisvali@yahoo.com>
2025-06-01 15:52:11 +0300

    add code for extracting data from playthroughs

--- ai/analyze_test.go
+++ ai/analyze_test.go
@@ -7,79 +7,173 @@ import (
 	"github.com/stretchr/testify/assert"
 	_ "image/png"
 	"os"
+	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 )
 
-func CooldownToSpeed(cooldown Int) Int {
-	x := I(300).Minus(cooldown)
-	speed := x.Times(I(90)).DivBy(I(290)).Plus(I(10))
-	return speed
+type PlayedLevel struct {
+	Name            string
+	PlayIdx         int
+	PlaythroughName string
 }
 
-func AmmoLimitToNFlames(ammoLimit Int) Int {
-	nFlames := ammoLimit.Minus(I(1)).DivBy(I(3)).Plus(I(1))
-	return nFlames
+func (p *PlayedLevel) IsTrainingLevel() bool {
+	return strings.Contains(p.Name, "training")
 }
 
+func (p *PlayedLevel) IsTestLevel() bool {
+	return strings.Contains(p.Name, "test")
+}
+
+func (p *PlayedLevel) ParamsFilename() string {
+	return fmt.Sprintf("%s.mln013-params", p.Name)
+}
+
+func (p *PlayedLevel) PlayParamsFilename() string {
+	return fmt.Sprintf("%02d-%s.mln013-params", p.PlayIdx, p.Name)
+}
+
+func (p *PlayedLevel) LevelFilename() string {
+	return fmt.Sprintf("%s.mln013-level", p.Name)
+}
+
+func (p *PlayedLevel) PlayLevelFilename() string {
+	return fmt.Sprintf("%02d-%s.mln013-level", p.PlayIdx, p.Name)
+}
+
+func (p *PlayedLevel) SourcePlaythroughFilename() string {
+	return fmt.Sprintf("%s.mln013", p.PlaythroughName)
+}
+
+func (p *PlayedLevel) PlayPlaythroughFilename() string {
+	return fmt.Sprintf("%02d-%s.mln013", p.PlayIdx, p.Name)
+}
+
+func (p *PlayedLevel) PlaythroughFilename() string {
+	return fmt.Sprintf("%s.mln013", p.Name)
+}
+
+// Code used to extract data from playthroughs for experiment performed using
+// miln version 13. This code is meant to be used once, for generating the data
+// and is very specific to this version, on purpose. In the future it can be
+// copy-pasted and modified as needed.
 func TestExtract(t *testing.T) {
-	// playthroughs
-	dir := os.DirFS("d:\\Miln\\playthroughs\\gabi-experiment1").(FS)
-	files := GetFiles(dir, ".", "*")
+	// dirs
+	playDir := "../data/levels"
+	playthroughsDir := "d:\\Miln\\playthroughs\\gabi-experiment1"
 
-	// list of levels with which the version was delivered, in order, with their
-	// names used when generated
-	files2 := GetFiles(os.DirFS("..").(FS), "data/levels", "*")
+	inputPlayDataDir := "d:\\Miln\\stored\\experiment2\\ai-input\\play-data"
+	inputTrainingDataDir := "d:\\Miln\\stored\\experiment2\\ai-input\\training-data"
+	inputTestDataDir := "d:\\Miln\\stored\\experiment2\\ai-input\\test-data"
 
-	// list of trainings
-	dirTrainingData := os.DirFS("d:\\Miln\\stored\\experiment2\\ai-input\\training-data").(FS)
-	filesTrainingParams := GetFiles(dirTrainingData, ".", "*.mln013-params")
+	outputPlayDataDir := "d:\\Miln\\stored\\experiment2\\ai-output\\play-data"
+	outputTrainingDataDir := "d:\\Miln\\stored\\experiment2\\ai-output\\training-data"
+	outputTestDataDir := "d:\\Miln\\stored\\experiment2\\ai-output\\test-data"
 
-	// list of test
-	// dirTestData := os.DirFS("d:\\Miln\\stored\\experiment2\\ai-input\\test-data").(FS)
-	// filesTestParams := GetFiles(dirTestData, ".", "*.mln013-params")
+	DeleteDir(outputPlayDataDir)
+	MakeDir(outputPlayDataDir)
 
-	for idx, file := range files {
-		if !strings.Contains(files2[idx], "training") {
-			continue
-		}
+	DeleteDir(outputTrainingDataDir)
+	MakeDir(outputTrainingDataDir)
 
-		file = file[2:]
-		data, err := dir.ReadFile(file)
+	DeleteDir(outputTestDataDir)
+	MakeDir(outputTestDataDir)
+
+	// Initialize list of played levels.
+	playFiles := GetFiles(os.DirFS(playDir).(FS), ".", "*")
+	playthroughFiles := GetFiles(os.DirFS(playthroughsDir).(FS), ".", "*")
+	playedLevels := make([]PlayedLevel, len(playFiles))
+	for idx := range playFiles {
+		playFilename := filepath.Base(playFiles[idx])
+		idxStr := playFilename[:2]
+		playIdx, err := strconv.Atoi(idxStr)
 		Check(err)
-		playthrough := DeserializePlaythrough(data)
-
-		var fileParams string
-		for i := range filesTrainingParams {
-			fileId := strings.Split(filesTrainingParams[i][2:], ".")[0]
-			if strings.Contains(files2[idx], fileId) {
-				fileParams = filesTrainingParams[i]
-			}
-		}
-		var params Param
-		LoadYAML(dirTrainingData, fileParams[2:], &params)
-
-		world := NewWorld(playthrough.Seed, playthrough.Level)
-		for _, input := range playthrough.History {
-			world.Step(input)
-		}
-
-		var result string
-		if world.Player.Health.IsPositive() {
-			result = "win"
-		} else {
-			result = "loss"
-		}
-
-		// Playthrough	NEnemies	EnemySpeed	NObstacles	NFlames	Result
-		fmt.Printf("%s,%d,%d,%d,%d,%s,%d\n",
-			file,
-			params.NEnemies.ToInt(),
-			params.EnemySpeed.ToInt(),
-			params.NObstacles.ToInt(),
-			params.NFlames.ToInt(),
-			result,
-			world.Player.Health.ToInt())
+		assert.Equal(t, playIdx, idx+1)
+		playedLevels[idx].PlayIdx = playIdx
+		playedLevels[idx].Name = strings.Split(playFilename, ".")[0][3:]
+		playthroughFilename := filepath.Base(playthroughFiles[idx])
+		playedLevels[idx].PlaythroughName = strings.Split(playthroughFilename, ".")[0]
 	}
-	assert.True(t, true)
+
+	// Copy files to output dirs.
+	var source, dest string
+	for idx := range playedLevels {
+		// Fill play data dir.
+		source = inputPlayDataDir + "/" + playedLevels[idx].PlayLevelFilename()
+		dest = outputPlayDataDir + "/" + playedLevels[idx].PlayLevelFilename()
+		CopyFile(source, dest)
+
+		if playedLevels[idx].IsTrainingLevel() {
+			source = inputTrainingDataDir + "/" + playedLevels[idx].ParamsFilename()
+		} else {
+			source = inputTestDataDir + "/" + playedLevels[idx].ParamsFilename()
+		}
+		dest = outputPlayDataDir + "/" + playedLevels[idx].PlayParamsFilename()
+		CopyFile(source, dest)
+
+		source = playthroughsDir + "/" + playedLevels[idx].SourcePlaythroughFilename()
+		dest = outputPlayDataDir + "/" + playedLevels[idx].PlayPlaythroughFilename()
+		CopyFile(source, dest)
+
+		if playedLevels[idx].IsTrainingLevel() {
+			// Fill training data dir.
+			source = inputTrainingDataDir + "/" + playedLevels[idx].LevelFilename()
+			dest = outputTrainingDataDir + "/" + playedLevels[idx].LevelFilename()
+			CopyFile(source, dest)
+
+			source = inputTrainingDataDir + "/" + playedLevels[idx].ParamsFilename()
+			dest = outputTrainingDataDir + "/" + playedLevels[idx].ParamsFilename()
+			CopyFile(source, dest)
+
+			source = playthroughsDir + "/" + playedLevels[idx].SourcePlaythroughFilename()
+			dest = outputTrainingDataDir + "/" + playedLevels[idx].PlaythroughFilename()
+			CopyFile(source, dest)
+		} else {
+			// Fill test data dir.
+			source = inputTestDataDir + "/" + playedLevels[idx].LevelFilename()
+			dest = outputTestDataDir + "/" + playedLevels[idx].LevelFilename()
+			CopyFile(source, dest)
+
+			source = inputTestDataDir + "/" + playedLevels[idx].ParamsFilename()
+			dest = outputTestDataDir + "/" + playedLevels[idx].ParamsFilename()
+			CopyFile(source, dest)
+
+			source = playthroughsDir + "/" + playedLevels[idx].SourcePlaythroughFilename()
+			dest = outputTestDataDir + "/" + playedLevels[idx].PlaythroughFilename()
+			CopyFile(source, dest)
+		}
+	}
+
+	// Extract data from playthroughs.
+	dirs := []string{outputPlayDataDir, outputTrainingDataDir, outputTestDataDir}
+	for _, dir := range dirs {
+		playthroughFiles := GetFiles(os.DirFS(dir).(FS), ".", "*.mln013")
+		paramsFiles := GetFiles(os.DirFS(dir).(FS), ".", "*.mln013-params")
+
+		outputFile, err := os.Create(dir + "/output.csv")
+		Check(err)
+		for i := range playthroughFiles {
+			var params Param
+			LoadYAML(os.DirFS(dir).(FS), paramsFiles[i][2:], &params)
+
+			playthrough := DeserializePlaythrough(ReadFile(dir + "/" + playthroughFiles[i]))
+			world := NewWorld(playthrough.Seed, playthrough.Level)
+			for _, input := range playthrough.History {
+				world.Step(input)
+			}
+
+			// Playthrough	NEnemies	EnemySpeed	NObstacles	NFlames	PlayerHealth
+			_, err := outputFile.WriteString(fmt.Sprintf("%s,%d,%d,%d,%d,%d\n",
+				filepath.Base(playthroughFiles[i]),
+				params.NEnemies.ToInt(),
+				params.EnemySpeed.ToInt(),
+				params.NObstacles.ToInt(),
+				params.NFlames.ToInt(),
+				world.Player.Health.ToInt()))
+			Check(err)
+		}
+		Check(outputFile.Close())
+	}
 }
