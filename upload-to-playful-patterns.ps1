a720ac16a7f8688a3a81d512fb799ed1053c7e2e
Vali Maris <marisvali@yahoo.com>
2025-06-21 21:41:21 +0300

    test that uploading to website still works

--- upload-to-playful-patterns.ps1
+++ upload-to-playful-patterns.ps1
@@ -1,9 +1,9 @@
 $Env:GOOS = 'js'
 $Env:GOARCH = 'wasm'
-go build -tags online,fixedlevels -o miln16.wasm github.com/marisvali/miln
+go build -tags online,fixedlevels,int_overflow_checks_disabled,world_debug_info_disabled -o miln999.wasm github.com/marisvali/miln
 Remove-Item Env:GOOS
 Remove-Item Env:GOARCH
 
 $client = New-Object System.Net.WebClient
 $client.Credentials = New-Object System.Net.NetworkCredential($Env:MILN_FTP_USER, $Env:MILN_FTP_PASSWORD)
-$client.UploadFile("ftp://ftp.playful-patterns.com/public_html/miln16.wasm", "miln16.wasm")
+$client.UploadFile("ftp://ftp.playful-patterns.com/public_html/miln999.wasm", "miln999.wasm")
