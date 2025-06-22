812d2f78adae2ea03e0e32698d84513189d9264c
Vali Maris <marisvali@yahoo.com>
2025-06-22 12:52:36 +0300

    add explanation for why arrays are sometimes better even if performance is not the main consideration

--- world/level.go
+++ world/level.go
@@ -5,6 +5,23 @@ import (
 	. "github.com/marisvali/miln/gamelib"
 )
 
+// Level is only meant to be an input for World. It is important for World to
+// use arrays, not slices, for performance reasons. Level isn't relevant to
+// performance yet, so it could use slices. Slices are treated nicely by the
+// YAML serialization library. The YAML is meant to be human-readable and it's
+// annoying to have full arrays in there that are mostly empty.
+// However, I also want to copy/clone the Playthrough, which implies the Level.
+// And if Level has slices I have to write clone logic for that. That's annoying
+// for me. It's actually easier to write MarshalYAML and UnmarshalYAML for the
+// 2 arrays that need it. I prefer to have special code for serializing to YAML,
+// and have a simpler version of a basic operation like copy/clone. This way I
+// can also easily include the Level in WorldDebugInfo if I want, and trust
+// that it gets copied quickly and correctly.
+// Playthrough.History is different, it makes sense to have it a slice because
+// during real playthroughs I don't know how many frames the player will use
+// so I would have to add extra logic to make sure I don't crash. But Level is
+// exactly the kind of structure where it makes sense to decide the maximum
+// size in advance.
 type Level struct {
 	WorldParams        `yaml:"WorldParams"`
 	Obstacles          MatBool                `yaml:"Obstacles"`
